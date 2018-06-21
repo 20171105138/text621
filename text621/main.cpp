3dd967d99218f3d8f7528953deb695b956a745a1
20171105138 <s20171105138@mac05.local>
2018-06-21 09:21:41 +0800

    Initial Commit

---
+++ text621/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  text621
+//
+//  Created by 夏宇航 on 18/6/21.
+//  Copyright © 2018年 夏宇航. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
